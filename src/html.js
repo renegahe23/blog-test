1421d9b834ceb246631d4beeaa32341992bd3ed9
renegahe23git config --global user.name renegahe23 <renegahe@gmail.com>
2020-01-28 18:58:38 -0600

    test
    test1

---
+++ src/html.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content="#000000" />
+        <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="%PUBLIC_URL%/apple-icon.png"
+        />
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
+        />
+        <link
+          href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+          rel="stylesheet"
+        />
+
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <noscript key="noscript" id="gatsby-noscript">
+          This app works best with JavaScript enabled.
+        </noscript>
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array
+};
